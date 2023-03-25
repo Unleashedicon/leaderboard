42d518c6cede23762e609f8ae4ccbea71d01bee7
unleashed <kkipkurui123@gmail.com>
2023-03-25 04:33:02 +0300

    add getscore.js

---
+++ src/modules/getscores.js
@@ -0,0 +1,62 @@
+const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+// Variables
+let gameId;
+
+// Functions
+const getScores = async () => {
+  try {
+    const response = await fetch(`${apiUrl}games/${gameId}/scores/`);
+    const data = await response.json();
+    const scoreboard = document.getElementById('scoreboard');
+    scoreboard.innerHTML = '';
+    data.result.forEach((score) => {
+      const li = document.createElement('li');
+      li.textContent = `${score.user}: ${score.score}`;
+      scoreboard.appendChild(li);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const addScore = async (name, score) => {
+  try {
+    const response = await fetch(`${apiUrl}games/${gameId}/scores/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: name,
+        score,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    getScores();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createGame = async (name) => {
+  try {
+    const response = await fetch(`${apiUrl}games/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+      }),
+    });
+    const data = await response.json();
+    gameId = data.result.substring(11, 33);
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getScores, addScore, createGame };
